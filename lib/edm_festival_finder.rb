5de44a34ec12ecc26f0d0f7780b4395bdb6da55c
Jonathan Luk <jluk87@gmail.com>
2018-04-23 00:53:10 -0400

    ran bundle gem to stub out gem files and set up the environment

---
+++ lib/edm_festival_finder.rb
@@ -0,0 +1,4 @@
+require_relative "edm_festival_finder/version"
+require_relative "edm_festival_finder/cli"
+require_relative "edm_festival_finder/festival"
+require_relative "edm_festival_finder/scraper"
