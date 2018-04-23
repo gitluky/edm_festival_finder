2eec98709f029b83c06f512cb4e1c2acbf31888b
Jonathan Luk <jluk87@gmail.com>
2018-04-23 19:13:37 -0400

    completed the first pass over the CLI class

--- lib/edm_festival_finder/cli.rb
+++ lib/edm_festival_finder/cli.rb
@@ -1,6 +1,6 @@
 class EdmFestivalFinder::CLI
 
-  attr_accessor :startdate, :enddate
+  attr_accessor :startdate, :enddate, :country_code, :festival_count, :festival_num
 
   def initialize
     self.startdate = Date.today.to_s
@@ -18,39 +18,80 @@ class EdmFestivalFinder::CLI
     puts ""
     puts "    [Press enter to choose country]"
     puts ""
+    input = gets.strip
+    while input != "\n"
+      input = gets.strip
+    end
+    self.choose_country
+    self.get_festivals
+    self.display_festivals
+    self.choose_festivals
+    self.festival_details
   end
 
-  def list_countries
+  def choose_country
     country_hash = EdmFestivalFinder::Scraper.scrape_countries
     country_hash.each do
       |country,code| puts " #{code} - #{country}"}
     end
-  end
-
-  def enter_country
     puts "Enter the country code for the country of choice listed above"
-    country_code = gets.strip
+    self.country_code = gets.strip
     while country_hash.none? {|country,code| input == code}
       puts "Country code was not invalid, please re-enter."
-      country_code = gets.strip
+      self.country_code = gets.strip
     end
-    EdmFestivalFinder::Scraper.scrape_festivals(country_code)
+  end
+
+  def get_festivals
+    EdmFestivalFinder::Festival.create_from_search(EdmFestivalFinder::Scraper.scrape_festivals(self.country_code))
+    EdmFestivalFinder::Festival.add_additional_details(EdmFestivalFinder::Scraper.scrape_individual_festivals(self.country_code))
+    self.display_festivals
   end
 
   def display_festivals
-    i = 1
+    self.festival_count = 0
     EdmFestivalFinder::Festival.all.each do |festival|
+      self.festival_count +=1
       puts "{i}. #{festival.name}"
       puts "  #{festival.location}, #{festival.startdate} - #{festival.enddate}"
-      i+=1
     end
-    puts "Enter the number for the festival to see more details"
-
   end
 
+  def choose_festival
+    puts "Enter the number for the festival to see more details or type 'back' or 'exit'"
+    self.festival_num = gets.strip
+    while !input.between?(1,self.festival_count) || input != 'back' || input != 'exit'
+      puts "Please re-enter the number, or type 'back' or 'exit'"
+    end
+    if input == 'back'
+      EdmFestivalFinder::Festival.all.reset_all
+      choose_country
+    else
+      puts "Thank you for using EDM Festival Finder, Goodbye."
+    end
+  end
 
-
-
-
+  def festival_details
+    puts "name: #{EdmFestivalFinder::Festival.all[self.festival_num].name}"
+    puts "location: #{EdmFestivalFinder::Festival.all[self.festival_num].location}"
+    puts "date: #{EdmFestivalFinder::Festival.all[self.festival_num].date}"
+    puts "attendance: #{EdmFestivalFinder::Festival.all[self.festival_num].attendance}"
+    puts "environment: #{EdmFestivalFinder::Festival.all[self.festival_num].environment}"
+    puts "type of event: #{EdmFestivalFinder::Festival.all[self.festival_num].type_of_event}"
+    puts "link #{EdmFestivalFinder::Festival.all[self.festival_num].link}"
+    puts "facebook #{EdmFestivalFinder::Festival.all[self.festival_num].facebook}"
+    puts ""
+    puts "Type 'back' to go to the festival list or 'exit'"
+    input = gets.strip
+    while input != 'back' || 'exit'
+      input = gets.strip
+    end
+    if input == 'back'
+      display_festivals
+    else
+      EdmFestivalFinder::Festival.all.reset_all
+      puts "Thank you for using EDM Festival Finder, Goodbye."
+    end
+  end
 
 end
